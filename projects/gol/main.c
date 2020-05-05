246495dcfdf6767086665d541e5a7e17c84ae9a7
Semih Ufuk Güler <sufukguler@gmail.com>
2020-05-05 12:47:37 +0300

    Update main.c

--- projects/gol/main.c
+++ projects/gol/main.c
@@ -3,6 +3,14 @@
 	Student ID   :
 */
 
+/*
+C   N                 new C
+   1   0,1             ->  0  # Lonely
+   1   4,5,6,7,8       ->  0  # Overcrowded
+   1   2,3             ->  1  # Lives
+   0   3               ->  1  # It takes three to give birth!
+   0   0,1,2,4,5,6,7,8 ->  0  # Barren
+	 */
 #include <stdlib.h>
 #include <stdio.h>
 #include "gol.h"
