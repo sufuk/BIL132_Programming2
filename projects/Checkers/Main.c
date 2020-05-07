fbb9b5c348efc05530328d92e94addbf48f5d7f2
Semih Ufuk Güler <sufukguler@gmail.com>
2020-05-07 11:41:31 +0300

    Beautified

--- projects/Checkers/Main.c
+++ projects/Checkers/Main.c
@@ -1,11 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "funcs.h"
-
-
 int main(){
 char *b = NULL;
-//char *lap = NULL;
 b = checkersboard();
 readboard(b);
 print_board(b);
@@ -16,9 +13,7 @@ int y1 = 3;
 int z;
 z = isvalidmove(b, 1, x, y, x1, y1);
 printf("%d\n", z);
-
 movement(b, 1, x, y, x1, y1);
-//destroy_board(b);
 printf("\n\n");
 print_board(b);
 movement(b, 1, 0, 3, 0, 4);
